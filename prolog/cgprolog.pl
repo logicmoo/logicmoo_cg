dd4cd30e724d15451d8b08c998b0cd97c3f7f1de
logicmoo <logicmoo@gmail.com>
2020-09-11 04:17:55 -0700

    initial CG-in-Prolog

---
+++ prolog/cgprolog.pl
@@ -0,0 +1,31 @@
+
+:- use_module(library(logicmoo_common)).
+
+:-multifile(cgr/3).
+:-multifile(cg/4).
+:-multifile(cgc/5).
+:-multifile(isa/2).
+:-multifile(ind/3).
+:-multifile(isa_kind/4).
+:-dynamic(cgr/3).
+:-dynamic(cg/4).
+:-dynamic(cgc/5).
+:-dynamic(isa/2).
+:-dynamic(ind/3).
+:-dynamic(isa_kind/4).
+
+
+:- expects_dialect(sicstus).
+:- ensure_loaded(cgprolog_reader).
+:- ensure_loaded(cgprolog_operations).
+:- ensure_loaded(cgprolog_translator).
+
+:- expects_dialect(cg).
+
+cg([Man:karim]<-agnt-[Eat]-obj->[Apple]).
+
+cg([Man:imad]<-agnt-[Drive]-obj->[Car]).
+
+cg([Man:karim]<-agnt-[Drink]-obj->[Water]).
+
+
