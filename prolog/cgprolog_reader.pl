a8a569e7718c84d07be8c67947aed90c65fe43c7
logicmoo <logicmoo@gmail.com>
2020-09-13 05:49:18 -0700

    muchop3

--- prolog/cgprolog_reader.pl
+++ prolog/cgprolog_reader.pl
@@ -68,7 +68,8 @@ dcg_look(Grammar,List,List):- (var(Grammar)->((N=2;N=1;between(3,20,N)),length(G
 parse_cg(List) --> concept(S),!, post_concept(S,List).
 
 post_concept(S,List) --> ['-'], dcg_look(['-']),!,graph_listnode(S,List).
-post_concept(Subj,[t(Rel,Subj,Obj)|List]) --> rel_right(Rel),!,concept(Obj),graph_listnode(Obj,List).
+post_concept(Subj,[t(Rel,Subj,Obj)|List]) --> rel_right2(Rel),!,concept(Obj),graph_listnode(Obj,List).
+post_concept(Subj,[t(Rel,Subj,Obj)|List]) --> rel_right(Rel),!,concept(Obj),graph_listnode(Subj,List).
 post_concept(Obj, [t(Rel,Subj,Obj)|List]) --> rel_left(Rel),!,concept(Subj),graph_listnode(Subj,List).
 
 graph_listnode(Subj,List) --> [','],!,graph_listnode(Subj,List).
@@ -79,6 +80,7 @@ graph_listnode(Obj,[t(Rel,Subj,Obj)|List]) --> rel_left(Rel), concept(Subj), gra
 graph_listnode(_,[])--> ((\+ [_]);['.']).
 
 rel_right(Rel)-->['-'],rel(Rel),['->'].
+rel_right2(Rel)-->['->'],rel(Rel),['->'].
 rel_left(Rel)-->['<-'],rel(Rel),['-'].
 
 rel(C)--> ['('],word_tok_loose(C),[')'].
@@ -86,13 +88,19 @@ rel(C)--> word_tok_loose(C).
 
 word_tok_loose(DC)-->[C],{atom(C),downcase_atom(C,DC)}.
 
+nonword_tok(X):- atom(X),upcase_atom(X,UC),downcase_atom(X,DC),!,UC==DC. 
 
+                                                  
+
+concept(co(Type,'#'(Num)))--> ['[', Type, ':', '#', Num, ']'],!.
+concept(co(Type,'#'(Num)))--> ['[', Type, '#', Num, ']'],!.
+concept(all(Type))--> ['[', Type, ':', '@', 'every', ']'],!.
 concept(entity(C))--> ['[',C,']'],!.
 concept(ct(Type,Word))--> ['[',Type,':',Word,']'],!.
 concept(cot(Type,OP,Word))--> ['[',Type,':',OP,Word,']'],!.
 concept(cot(Type,OP,Word))--> ['[',Type,OP,Word,']'],!.
 concept(ct(Type,Word))--> ['[',Type,Word,']'],!.
-concept(cg(Concept,SubGraph))--> ['[',Concept,'='], parse_cg(SubGraph),[']'],!.
+concept(cg(Concept,SubGraph))--> ['[',Concept,'='], parse_cg(SubGraph), [']'],!.
 concept(?(Var)) --> [?(Var)].
 
 cg_df_to_term(In,Out):- any_to_string(In,Str),
@@ -114,45 +122,29 @@ cmt_until_eoln(`;;`).
 cmt_until_eoln(`%`).
 
 
+cg_test_data([reader, level(0)], "[Cat: @every]-(On)->[Mat]").
 cg_test_data([reader, level(0)], "[Mat #1]-(equal)->[Thingy #1]").
 cg_test_data([reader, level(1)], "['Man':imad]<-agnt-['Drive']-obj->['Car']").
-cg_test_data([reader, level(1)], "[Cat: #1]-(On)->[Mat #1]-(equal)->[Thingy #1]").
+cg_test_data([reader, level(1)], "[Cat#1]-(On)->[Mat #1]-(equal)->[Thingy #1]").
 cg_test_data([reader, level(1)], "[Cat: ?x]-(equal)->M1-(On)->[Mat]").
 cg_test_data([reader, level(1)], "[Cat: ?x]-(On)->[Mat]").
 cg_test_data([reader, level(1)], "[Man:karim]<-agnt-[Drink]-obj->[Water]").
-cg_test_data([reader, level(1)], "[Mat #1]<- (on)- [Cat: #1]").
+cg_test_data([reader, level(1)], "[Mat #1]<- (on)- [Cat #1]").
 cg_test_data([reader, level(1)], "[Mat]<-(On)-[Cat: ?x]").
 cg_test_data([reader, level(1)], "[Thingy #1]<-(equal)-[Mat #1]").
 cg_test_data([reader, level(2)], "[Cat #1]-(On)->[Mat #1]-(equal)->[Thingy #1]").
 cg_test_data([reader, level(2)], "[Man:karim]<-agnt-[Drink]-obj->[Water]").
-cg_test_data([reader, level(2)], "[Thingy #1] <- (equal) -[Mat #1]<- (on)- [Cat: #1]").
+cg_test_data([reader, level(2)], "[Thingy #1] <- (equal) -[Mat #1]<- (on)- [Cat#1]").
 cg_test_data([reader, level(3)], "[Cat: @every]->(On)->[Mat]").
+cg_test_data([reader, level(3)], "[Go*x][Person:'John'*y][City:'Boston'*z][Bus*w](Agnt?x?y)(Dest?x?z)(Inst?x?z)").
+cg_test_data([xcall, level(0), funky_syntax], "?x -(equal)-> [Thingy #1]").
 
-cg_test_data([reader, level(3)], "
- [Go2]
-   - (Agnt)->[Person: John2]
-   - (Dest)->[City: Boston2]
-   - (Inst)->[Bus2])").
-
-cg_test_data([reader, level(3)], "[Woman:red]<-knows-[Man:karim]<-agnt-[Eat]-obj->[Apple]-(on)->table").
-
-cg_test_data([reader, level(3)], "
-[Begin]-
-        -obj->[Session],
-        -srce->[Proposition = [Press] -
-       -obj -> [Key : enter]-partOf->[Keyboard],
-       -agnt -> [Person : John] ],
-        -agnt->[Person : John]").
-
-cg_test_data([reader, level(4)], "
-   [Person: John2] <- (Agnt) - 
-   [City: Boston2] <- (Dest) -
-   [Bus2] <- (Inst) - [Go2])").
-
-cg_test_data([reader, level(4)], "
-[Person: Tom]<-(Expr)<-[Believe]->(Thme)-
-     [Proposition:  [Person: Mary *x]<-(Expr)<-[Want]->(Thme)-
-     [Situation:  [?x]<-(Agnt)<-[Marry]->(Thme)->[Sailor] ]]").
+cg_test_data([xcall, level(0), funky_syntax], "?x -(On)->[Mat #1]-(equal)->[Thingy #1]").
+cg_test_data([xcall, level(0), funky_syntax], "?x -(On)->[Mat #1]").
+cg_test_data([xcall, level(0), funky_syntax], "[?x] -(equal)-> [Thingy #1]").
+cg_test_data([xcall, level(0), funky_syntax], "[?x]-(On)->[Mat #1]-(equal)->[Thingy #1]").
+cg_test_data([xcall, level(0), funky_syntax], "[Mat ?x]-(equal)->[Thingy #1]").
+cg_test_data([xcall, level(0)], "[Cat: ?x]-(On)->[Mat #1]-(equal)->[Thingy #2]").
 
 cg_test_data([reader, level(4)], "
 
@@ -161,21 +153,40 @@ cg_test_data([reader, level(4)], "
    (Dest)->[City: Boston] -
    (Inst)->[Bus]").
 
+
+cg_test_data([reader, level(3)], "
+ [a] - (belives) -> 
+ [statement = [Go2]
+   - (Agnt)->[Person: John2]
+   - (Dest)->[City: Boston2]
+   - (Inst)->[Bus2]  ]").
+
+cg_test_data([reader, level(3)], "[Woman:red]<-knows-[Man:karim]<-agnt-[Eat]-obj->[Apple]-(on)->table").
+
+skip_cg_test_data([reader, level(3)], "
+[Begin]-
+        -obj->[Session],
+        -srce->[Proposition = [Press] -
+       -obj -> [Key : enter]-partOf->[Keyboard],
+       -agnt -> [Person : John] ],
+        -agnt->[Person : John]").
+
 skip_cg_test_data([reader, level(4)], "
+   [Person: John2] <- (Agnt) - 
+   [City: Boston2] <- (Dest) -
+   [Bus2] <- (Inst) - [Go2])").
+
+skip_cg_test_data([reader, level(4)], "
+[Person: Tom]<-(Expr)<-[Believe]->(Thme)-
+     [Proposition:  [Person: Mary *x]<-(Expr)<-[Want]->(Thme)-
+     [Situation:  [?x]<-(Agnt)<-[Marry]->(Thme)->[Sailor] ]]").
+     skip_cg_test_data([reader, level(4)], "
 // ontology required (to load first): aminePlatform/samples/ontology/ManOntology2.xml
 [Eat #0] -
    - obj->[Apple],
    - manr->[Fast],
    - agnt->[Man]").
 
-cg_test_data([xcall, level(0), funky_syntax], "?x -(equal)-> [Thingy #1]").
-
-cg_test_data([xcall, level(0), funky_syntax], "?x -(On)->[Mat #1]-(equal)->[Thingy #1]").
-cg_test_data([xcall, level(0), funky_syntax], "?x -(On)->[Mat #1]").
-cg_test_data([xcall, level(0), funky_syntax], "[?x] -(equal)-> [Thingy #1]").
-cg_test_data([xcall, level(0), funky_syntax], "[?x]-(On)->[Mat #1]-(equal)->[Thingy #1]").
-cg_test_data([xcall, level(0), funky_syntax], "[Mat ?x]-(equal)->[Thingy #1]").
-cg_test_data([xcall, level(0)], "[Cat: ?x]-(On)->[Mat #1]-(equal)->[Thingy #1]").
 
 :- pop_operators.
 
